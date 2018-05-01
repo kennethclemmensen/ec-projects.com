2b3e8ccefa1dbbff261ef164e7b7d0a8a55a6025
Kenneth Clemmensen <kenneth.clemmensen@gmail.com>
2018-05-01 20:38:57 +0200

    Styling of the theme

--- public/themes/ec-projects/js/script.js
+++ public/themes/ec-projects/js/script.js
@@ -1,6 +1,23 @@
 jQuery.noConflict();
 (function($) {
     $(document).ready(function() {
-
+        $('.header__nav-trigger').on('click', function(event) {
+            event.preventDefault();
+            $('html, body').toggleClass('show-mobile-navigation');
+            $('.mobile-navigation').toggleClass('mobile-navigation--active');
+        });
+        var $header = $('.header');
+        var headerScrollClass = 'header--scroll';
+        var $navigationContainer = $('.navigation__container');
+        var navigationScrollClass = 'navigation__container--scroll';
+        $(window).scroll(function() {
+            if(window.pageYOffset > 0) {
+                $header.addClass(headerScrollClass);
+                $navigationContainer.addClass(navigationScrollClass);
+            } else {
+                $header.removeClass(headerScrollClass);
+                $navigationContainer.removeClass(navigationScrollClass);
+            }
+        });
     });
 })(jQuery);
