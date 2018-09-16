8369192effbc0adec2fca263738cc37cfd9d73c4
Kenneth Clemmensen <kenneth.clemmensen@gmail.com>
2018-09-16 18:08:32 +0200

    Styling of the theme

--- public/themes/ec-projects/js/script.js
+++ public/themes/ec-projects/js/script.js
@@ -6,6 +6,21 @@ jQuery.noConflict();
             $('html, body').toggleClass('show-mobile-navigation');
             $('.mobile-navigation').toggleClass('mobile-navigation--active');
         });
+        $('#slider').slick({
+            arrows: false,
+            autoplay: true,
+            mobileFirst: true,
+            responsive: [{
+                breakpoint: 767,
+                settings: {
+                    appendArrows: $('.slider__arrows'),
+                    arrows: true,
+                    autoplay: false,
+                    prevArrow: '<span class="slider__arrow"></span>',
+                    nextArrow: '<span class="slider__arrow slider__arrow--next"></span>'
+                }
+            }]
+        });
         var $header = $('.header');
         var headerScrollClass = 'header--scroll';
         $(window).scroll(function() {
