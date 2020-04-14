2725279b5b51e7a8e2cfc86de7455311f68d855f
Kenneth Clemmensen <kenneth.clemmensen@gmail.com>
2020-04-14 16:14:11 +0200

    Updated script.js

--- public/themes/custom/ec-projects/js/script.js
+++ public/themes/custom/ec-projects/js/script.js
@@ -1,30 +1,27 @@
-jQuery.noConflict();
-(function($) {
-    $(() => {
-        $('.header__nav-trigger').on('click', (event) => {
-            event.preventDefault();
-            $('html, body').toggleClass('show-mobile-navigation');
-            $('.mobile-navigation').toggleClass('mobile-navigation--active');
-        });
-        $('#slider').slick({
-            arrows: false,
-            autoplay: true,
-            mobileFirst: true,
-            responsive: [{
-                breakpoint: 767,
-                settings: {
-                    appendArrows: $('.slider__arrows'),
-                    arrows: true,
-                    autoplay: false,
-                    prevArrow: '<span class="slider__arrow"></span>',
-                    nextArrow: '<span class="slider__arrow slider__arrow--next"></span>'
-                }
-            }]
-        });
-        let $header = $('.header');
-        let headerScrollClass = 'header--scroll';
-        $(window).scroll(() => {
-            (window.pageYOffset > 0) ? $header.addClass(headerScrollClass) : $header.removeClass(headerScrollClass);
-        });
+document.addEventListener('DOMContentLoaded', () => {
+    $('.header__nav-trigger').on('click', (event) => {
+        event.preventDefault();
+        $('html, body').toggleClass('show-mobile-navigation');
+        $('.mobile-navigation').toggleClass('mobile-navigation--active');
     });
-})(jQuery);
+    $('#slider').slick({
+        arrows: false,
+        autoplay: true,
+        mobileFirst: true,
+        responsive: [{
+            breakpoint: 767,
+            settings: {
+                appendArrows: $('.slider__arrows'),
+                arrows: true,
+                autoplay: false,
+                prevArrow: '<span class="slider__arrow"></span>',
+                nextArrow: '<span class="slider__arrow slider__arrow--next"></span>'
+            }
+        }]
+    });
+    let $header = $('.header');
+    let headerScrollClass = 'header--scroll';
+    $(window).scroll(() => {
+        (window.pageYOffset > 0) ? $header.addClass(headerScrollClass) : $header.removeClass(headerScrollClass);
+    });
+});
