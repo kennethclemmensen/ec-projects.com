a3afdff19d8781d8b8e0773009bec7d5e231b47a
Kenneth Clemmensen <kenneth.clemmensen@gmail.com>
2020-06-09 15:32:16 +0200

    Drupal is updated to version 9.0.0

--- public/vendor/autoload.php
+++ public/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInitbd3d4f4ee12b0f17896172b1eebb86ad::getLoader();
+return ComposerAutoloaderInit9ff8aabc0b86727809e283c548031759::getLoader();
