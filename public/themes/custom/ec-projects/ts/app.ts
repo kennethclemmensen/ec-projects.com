b337a6cc86e466044da449c38cb26c409b430df8
Kenneth Clemmensen <kenneth.clemmensen@gmail.com>
2025-05-31 05:41:27 +0200

    The files eventType.ts and slider.ts are deleted

--- public/themes/custom/ec-projects/ts/app.ts
+++ public/themes/custom/ec-projects/ts/app.ts
@@ -1,26 +1,53 @@
 import { fromEvent, tap } from 'rxjs';
-import { EventType } from './eventType';
-import { Slider } from './slider';
 
-fromEvent(document, EventType.DOMContentLoaded).pipe(
+fromEvent(document, 'DOMContentLoaded').pipe(
     tap(() => {
         const headerNavTrigger = document.getElementsByClassName('header__nav-trigger')[0];
         const header = document.getElementsByClassName('header')[0];
-        const headerScrollClass = 'header--scroll';
         const showMobileMenuClass = 'show-mobile-menu';
-        if (headerNavTrigger != null) {
-            fromEvent(headerNavTrigger, EventType.Click).subscribe((event) => {
-                event.preventDefault();
-                document.getElementsByTagName('html')[0]?.classList.toggle(showMobileMenuClass);
-                document.getElementsByTagName('body')[0]?.classList.toggle(showMobileMenuClass);
-                document.getElementsByClassName('mobile-menu')[0]?.classList.toggle('mobile-menu--active');
-            });
-        }
-        fromEvent(window, EventType.Scroll).subscribe(() => {
-            if (header != null) {
-                (window.scrollY > 0) ? header.classList.add(headerScrollClass) : header.classList.remove(headerScrollClass);
-            }
+        const slides = document.querySelectorAll<HTMLElement>('.slider__slide');
+        const numberOfSlides = slides.length;
+        const previousArrow = document.getElementById('previous-arrow');
+        const nextArrow = document.getElementById('next-arrow');
+        let currentSlideIndex = 0;
+        slides.forEach((slide, i) => {
+            slide.style.transform = `translateX(${i * 100}%)`;
         });
-        new Slider();
+        fromEvent(window, 'scroll').pipe(
+            tap(() => {
+                if (header != null) {
+                    header.classList.toggle('header--scroll', window.scrollY > 0);
+                }
+            })
+        ).subscribe();
+        if(headerNavTrigger != null && previousArrow != null && nextArrow != null) {
+            fromEvent(headerNavTrigger, 'click').pipe(
+                tap((event) => {
+                    event.preventDefault();
+                    document.getElementsByTagName('html')[0]?.classList.toggle(showMobileMenuClass);
+                    document.body.classList.toggle(showMobileMenuClass);
+                    document.getElementsByClassName('mobile-menu')[0]?.classList.toggle('mobile-menu--active');
+                })
+            ).subscribe();
+            fromEvent([previousArrow, nextArrow], 'click').pipe(
+                tap((event) => {
+                    if (event.target === previousArrow) {
+                        currentSlideIndex--;
+                        if(currentSlideIndex < 0) {
+                            currentSlideIndex = numberOfSlides - 1;
+                        }
+                    } else {
+                        currentSlideIndex++;
+                        if(currentSlideIndex === numberOfSlides) {
+                            currentSlideIndex = 0;
+                        }
+                    }
+                    slides.forEach((slide, i) => {
+                        slide.style.transform = `translateX(${(i - currentSlideIndex) * 100}%)`;
+                        slide.style.transition = 'transform 0.5s ease';
+                    });
+                })
+            ).subscribe();
+        }
     })
 ).subscribe();
--- public/themes/custom/ec-projects/ts/eventType.ts
+++
@@ -1,8 +0,0 @@
-/**
- * The EventType enum defines the different types of events
- */
-export enum EventType {
-    Click = 'click',
-    DOMContentLoaded = 'DOMContentLoaded',
-    Scroll = 'scroll'
-}
